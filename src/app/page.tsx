2cf102e2d1dfcf1936f0c0e745937da641d7eacb
Pasha0923 <alexsai@i.ua>
2024-09-26 20:53:59 +0300

    add components header , companyTable, companyRow, Toolbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import StatusLabel, { Status } from '@/app/components/StatusLabel';
+import { headers } from 'next/headers';
+import AddCompanyButton from './components/AddCompanyButton';
 // import AddCompanyButton from './components/AddCompanyButton';
 
 export default function Home() {
+  console.log(headers());
   return (
     <main>
-      <h1 className="text-xl">Home page</h1>
+      <h1 className="text-xl">Home page{new Date().toTimeString()}</h1>
+      <AddCompanyButton />
       <StatusLabel status={Status.Active}>Active</StatusLabel>
       <StatusLabel status={Status.NotActive}>Not Active</StatusLabel>
       <StatusLabel status={Status.Pending}>Pending</StatusLabel>
